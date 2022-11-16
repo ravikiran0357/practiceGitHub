35899034911c246069396f2f0e01ab9c24f7dc6a
ravikiran <ravikiran@lenovo-ThinkPad-E480>
2022-11-16 17:23:02 +0530

    ravi

--- Git/src/main/java/com/example/demo/hi.java
+++ Git/src/main/java/com/example/demo/hi.java
@@ -4,12 +4,7 @@ public class hi
 {
 	String nkjdd="first";
 	
-	int k=40;
 	
-	int i=10;
-	int j=40;
-	String name1="pr";
-	String commit="second";
 	
 	
 }
--- Git/src/main/java/com/example/demo/newclass.java
+++ Git/src/main/java/com/example/demo/newclass.java
@@ -4,7 +4,12 @@ public class newclass
 {
 	String changes="changes";
    
+int k=40;
 	
+	int i=10;
+	int j=40;
+	String name1="pr";
+	String commit="second";
 	public static void main(String[] args) {
 	String i="ffd";
 	}
