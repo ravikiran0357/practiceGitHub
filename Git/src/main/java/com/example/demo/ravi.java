6d94b1b057bbdc3d57855a7d9ba68e8621feb51d
ravikiran <ravikiran@lenovo-ThinkPad-E480>
2022-11-17 11:35:12 +0530

    changes by ravi

---
+++ Git/src/main/java/com/example/demo/ravi.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class ravi
+{
+
+	String changes="changes";
+   
+int k=40;
+	
+	int i=10;
+	int j=40;
+	String name1="pr";
+	String commit="second";
+	public static void main(String[] args) {
+	String i="ffd";
+	}
+	
+	public void prasu() {
+		System.out.println("ppr");
+		System.out.println("ppr");
+	}
+	public int intuuu() {
+		return 10;
+	}
+	
+
+}
